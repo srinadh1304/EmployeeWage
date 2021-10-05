223b0efe898ebc5d544f8aa29f4280221b2622bb
srinadh1304 <srinadh1304@gmail.com>
2021-10-05 21:46:07 +0530

    MODIFIED: Compute wages till working hours or days is reached

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -6,8 +6,11 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HOURS_IN_MONTH = 100;
 
 let employeeHours = 0;
+let totalEmployeeHours = 0;
+let totalWorkingDays = 0;
 
 function getWorkingHours(employeeCheck) {
 
@@ -27,10 +30,11 @@ function getWorkingHours(employeeCheck) {
     }
 }
 
-for(let day = 0; day<NUM_OF_WORKING_DAYS; day++){
+while(totalEmployeeHours <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
     employeeCheck = Math.floor(Math.random() * 10) % 3;
-    employeeHours += getWorkingHours(employeeCheck);
+    totalEmployeeHours += getWorkingHours(employeeCheck);
 }
 
-let employeeWage = employeeHours * WAGE_PER_HOUR;
-console.log("Total Working Hours = "+ employeeHours + "\nEmployee Wage = " + employeeWage);
+let employeeWage = totalEmployeeHours * WAGE_PER_HOUR;
+console.log("\nTotal Working Days = " + totalWorkingDays + "\nTotal Working Hours = " + totalEmployeeHours + "\nEmployee Wage = " + employeeWage);
