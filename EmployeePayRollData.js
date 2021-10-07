9e9d4e2e3e85b5138fc3de0e49204b89d3cecf78
srinadh1304 <srinadh1304@gmail.com>
2021-10-07 20:39:29 +0530

    MODIFIED: added new attributes to store employee data

--- EmployeePayRollData.js
+++ EmployeePayRollData.js
@@ -2,14 +2,30 @@ class EmployeePayrollData
 {
     id;
     salary;
+    name;
+    gender;
+    startDate;
 
-    constructor(id,name,salary)
+    constructor(...params) 
     {
-        this.id=id;
-        this.name=name;
-        this.salary=salary;
+        this.id=params[0];
+        this.name=params[1];
+        this.salary=params[2];
+        this.gender=params[3];
+        this.startDate=params[4];
+        
+    }
+
+
+    get id()
+    {
+        return this._id;
     }
     
+    set id(id)
+    {
+        this._id=id;
+    }
     get name()
     {
         return this._name;
@@ -20,12 +36,47 @@ class EmployeePayrollData
         this._name=name;
     }
 
+    get salary()
+    {
+        return this._salary;
+    }
+    
+    set salary(salary)
+    {
+        this._salary=salary;
+    }
+    get gender()
+    {
+        return this._gender;
+    }
+    
+    set gender(gender)
+    {
+        this._gender=gender;
+    }
+    get startDate()
+    {
+        return this._startDate;
+    }
+    
+    set startDate(startDate)
+    {
+        this._startDate=startDate;
+    }
+
+
     toString()
     {
-      return "id="+this.id+",name='"+this.name+",salary="+this.salary;  
+        const options={year:'numeric',month:'long',day:'numeric'};
+        const employeeDate=this.startDate==undefined?"undefined":
+                           this.startDate.toLocaleDateString("en-us",options);
+      return "id="+this.id+",name='"+this.name+",salary="+this.salary+
+              ",gender= "+this.gender+",startDate= "+employeeDate;  
     }
 }
 let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000);
 console.log(employeePayrollData.toString());
 employeePayrollData.name = "John"
 console.log(employeePayrollData.toString());
+let newEmployeePayrollData= new EmployeePayrollData(1,"Terisa",30000,"F",new Date());
+console.log(newEmployeePayrollData.toString());
